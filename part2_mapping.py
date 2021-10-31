a66e2d2179253c0dfbd853209880422105727f43
Corona Tsai <cstsai2@illinois.edu>
2021-10-31 01:49:49 -0500

    map

---
+++ part2_mapping.py
@@ -0,0 +1,34 @@
+import picar_4wd as fc
+from picar_4wd.pin import Pin
+from picar_4wd.ultrasonic import Ultrasonic
+import numpy as np
+import sys
+
+ua = Ultrasonic(Pin('D8'), Pin('D9'))
+
+def generate_map():
+    angles = [-90, -75, -60, -45, -30, -15, 0, 15, 30, 45, 60, 75, 90]
+    # Scan the surroundings
+    # store results as array of [angle, distance]
+    raw_data = []
+    for angle in angles:
+        distance = ua.get_distance_at(angle)
+        if distance < 0:
+            distance = 999
+        raw_data.append([angle, distance])
+    print(raw_data)
+    # Process array
+    # [0/1 , y, x]
+    #  50*sqrt(2) approx 71
+    # points = []
+    # for d in distances:
+    #
+    #
+    # # Fill out numpy array
+    # map = np.zeros((50,100))
+    #
+    # np.set_printoptions(threshold=sys.maxsize)
+    # print(arr)
+
+if __name__ == '__main__':
+    generate_map()
