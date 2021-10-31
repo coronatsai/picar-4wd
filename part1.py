cea012444601c0ea1c3ced7063b9b5731142e5d8
Corona Tsai <cstsai2@illinois.edu>
2021-10-31 03:08:42 -0500

    map

--- part1.py
+++ part1.py
@@ -6,33 +6,9 @@ from picar_4wd.pin import Pin
 from picar_4wd.ultrasonic import Ultrasonic
 import time
 speed = 25
-too_close = 30
-threshold = 50
+threshold = 40
 # create an Ultrasonic object
 ua = Ultrasonic(Pin('D8'), Pin('D9'))
-
-def avoid_obstacle():
-    distance = ua.get_distance()
-    # back away from obstacle if too close
-    while(distance < too_close):
-        time.sleep(0.04)
-        fc.backward(speed)
-        distance = ua.get_distance()
-
-    while(1):
-        # default is try to go right around the obstacle
-        fc.turn_right(speed)
-        distance = ua.get_distance()
-        if distance >= threshold:
-            fc.forward(speed)
-            fc.turn_left(speed)
-            left_dist = ua.get_distance()
-            if left_dist > distance:
-                break
-            fc.turn_right(speed)
-        distance = ua.get_distance()
-        if distance >= threshold:
-            break
 
 # keep driving straight, if an obstacle is "seen"
 # stop the vehicle, turn, and check for obstacle again
@@ -45,8 +21,10 @@ def main():
 
         if distance < 0:
             continue
+        elif distance < 20:
+            fc.backward(speed)
         elif distance < threshold:
-            avoid_obstacle()
+            fc.turn_right(speed)
         else:
             fc.forward(speed)
 
