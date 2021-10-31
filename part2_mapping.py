99121591c17bbb3b9003912c69ce826006dee21d
Corona Tsai <cstsai2@illinois.edu>
2021-10-31 01:56:30 -0500

    map

--- part2_mapping.py
+++ part2_mapping.py
@@ -1,10 +1,22 @@
 import picar_4wd as fc
 from picar_4wd.pin import Pin
+from picar_4wd.pwm import PWM
 from picar_4wd.ultrasonic import Ultrasonic
+from picar_4wd.servo import Servo
 import numpy as np
+import time
 import sys
 
 ua = Ultrasonic(Pin('D8'), Pin('D9'))
+servo = Servo(PWM("P0"), offset=0)
+# from init.py
+def get_distance_at(angle):
+    global angle_distance
+    servo.set_angle(angle)
+    time.sleep(0.04)
+    distance = us.get_distance()
+    angle_distance = [angle, distance]
+    return distance
 
 def generate_map():
     angles = [-90, -75, -60, -45, -30, -15, 0, 15, 30, 45, 60, 75, 90]
