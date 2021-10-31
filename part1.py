2fe883c58d719d89c005c0de642f25c0100cda63
Corona Tsai <cstsai2@illinois.edu>
2021-10-30 23:58:16 -0500

    part1

--- part1.py
+++ part1.py
@@ -1,2 +1,33 @@
 # Part 1
 
+# Car Can "See" and "Navigate" around obstables
+import picar_4wd as fc
+from picar_4wd.Ultrasonic import Ultrasonic
+
+speed = 20
+# create an Ultrasonic object
+ua = Ultrasonic(Pin('D8'), Pin('D9'))
+
+# keep driving straight, if an obstacle is "seen"
+# stop the vehicle, turn, and check for obstacle again
+# if no obstacle, keep driving
+
+def main():
+    while True:
+        distance = ua.get_distance()
+
+        print(distance)
+
+        if distance < 10:
+            fc.backward(speed)
+            fc.turn_right(speed)
+        elif distance < 30:
+            fc.turn_right(speed)
+        else:
+            fc.forward(speed)
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        fc.stop()
